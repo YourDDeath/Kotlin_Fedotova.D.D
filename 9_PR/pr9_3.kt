ea90d6aeb06a630dbe526158fc125e26bd693bcb
This_is_the_base <darina260806@gmail.com>
2024-10-03 20:10:15 +0900

    Add files via upload

---
+++ 9_PR/pr9_3.kt
@@ -0,0 +1,5 @@
+fun main() {
+    print("Введите сумму в рублях: ")
+    val rub = readln().toDouble()
+    println("Сумма в вонах: ${rub * 13.80}")
+}
---
+++ 9_PR/pr9_7.kt
@@ -0,0 +1,13 @@
+fun main() {
+    println("Введите строку: ")
+    val str = readln()
+    case(str)
+}
+
+fun case(str: String) {
+    val caseStr = str.map {
+        if (it.isUpperCase()) it.lowercaseChar()
+        else it.uppercaseChar()
+    }.joinToString("")
+    return println(caseStr)
+}
